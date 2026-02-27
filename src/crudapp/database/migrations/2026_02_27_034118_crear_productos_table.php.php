3052d50b309fa914a54dbe9d900d8dca8bd81463
Andres Jimenez <AF_JIMENEZ_SANTAMARIA@HOTMAIL.COM>
2026-02-26 22:58:03 -0500

    He agregado una nueva migracion paar poder crear las tablas de los productos y dos ejemplos por medio de comandos artisan

---
+++ src/crudapp/database/migrations/2026_02_27_034118_crear_productos_table.php.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //CREAR TABLA DE PRODUCTOS
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->decimal('precio', 8, 2);
+            $table->integer('stock');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+
+        //AGREGAR DATOS A LA TABLA DE PRODUCTOS
+        DB::table('productos')->insert([
+            [
+                'nombre' => 'Cepillo de dientes',
+                'descripcion' => 'Cepillo color azul para dientes',
+                'precio' => 1.99,
+                'stock' => 10,
+                'estado' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nombre' => 'Enjuague bucal',
+                'descripcion' => 'Enjuague bucal para dientes',
+                'precio' => 9.50,
+                'stock' => 5,
+                'estado' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //BORRAR TABLA DE PRODUCTOS
+        Schema::dropIfExists('productos');
+    }
+};
